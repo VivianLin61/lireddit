99b81c7859ff8e4e9b2f7bb28717eed07a3a85da
VivianLin61 <linvivian61@gmail.com>
2023-04-17 18:49:46 -0400

    login and register resolvers

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
+import { UserResolver } from './resolvers/user';
 // import { Post } from './entities/Post';
 
 const main = async () => {
@@ -14,9 +15,8 @@ const main = async () => {
   const app = express();
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
-      
     }),
     context: () => ({ em: orm.em }),
   });
