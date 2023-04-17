6077fe908e0d174859fa2eafb587c59f9c9a30e5
VivianLin61 <linvivian61@gmail.com>
2023-04-17 19:26:40 -0400

    set up cookies

--- src/index.ts
+++ src/index.ts
@@ -7,23 +7,62 @@ import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
 import { UserResolver } from './resolvers/user';
-// import { Post } from './entities/Post';
+import cors from 'cors';
+import RedisStore from 'connect-redis';
+import session from 'express-session';
+import { createClient } from 'redis';
+import { MyContext } from './types';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
   const app = express();
+  // Initialize client.
+  let redisClient = createClient();
+  redisClient.connect().catch(console.error);
+
+  // Initialize store.
+  let redisStore = new RedisStore({
+    client: redisClient,
+    disableTouch: true,
+    prefix: 'myapp:',
+  });
+
+  // Initialize sesssion storage.
+  app.use(
+    session({
+      name: 'qid',
+      store: redisStore,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        sameSite: 'lax', // csrf
+        httpOnly: true,
+        secure: __prod__, // cookie only works in https
+      },
+      resave: false, // required: force lightweight session keep alive (touch)
+      saveUninitialized: false, // recommended: only save session when data exists
+      secret: 'safjaldfjlkjcjiojeqw',
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
+
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
   await apolloServer.start();
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({
+    app,
+    cors: {
+      origin: ['http://localhost:3000'],
+      credentials: true,
+    },
+  });
 
   app.listen(4000, () => {
     console.log('server started on localhost:4000');
