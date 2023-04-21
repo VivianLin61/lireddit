c313ddb0ae3389d3f124c1cd3400f23e8c4a2448
VivianLin61 <linvivian61@gmail.com>
2023-04-21 17:18:00 -0400

    edit post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -151,20 +151,24 @@ export class PostResolver {
   }
 
   @Mutation(() => Post, { nullable: true })
+  @UseMiddleware(isAuth)
   async updatePost(
-    @Arg('id') id: number,
-    @Arg('title', () => String, { nullable: true }) title: string,
-    @Arg('title', () => String, { nullable: true }) text: string
+    @Arg('id', () => Int) id: number,
+    @Arg('title') title: string,
+    @Arg('text') text: string,
+    @Ctx() { req }: MyContext
   ): Promise<Post | null> {
-    const post = await Post.findOne({ where: { id } });
-    if (!post) {
-      return null;
-    }
-    if (typeof title !== 'undefined') {
-      post.title = title;
-      Post.update({ id }, { title });
-    }
-    return post;
+    const result = await AppDataSource.getRepository(Post)
+      .createQueryBuilder()
+      .update(Post)
+      .set({ title, text })
+      .where('id = :id and "creatorId" = :creatorId', {
+        id,
+        creatorId: req.session.userId,
+      })
+      .returning('*')
+      .execute();
+    return result.raw[0];
   }
 
   @Mutation(() => Boolean)
